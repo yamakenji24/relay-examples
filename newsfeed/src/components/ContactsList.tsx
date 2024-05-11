47e7862c5a4b5606e0f59c4f1dbcfb397d1e953a
yamakenji24 <yamakenji24@gmail.com>
2024-05-12 02:40:05 +0900

    dn: refetchable fragments

--- newsfeed/src/components/ContactsList.tsx
+++ newsfeed/src/components/ContactsList.tsx
@@ -1,17 +1,23 @@
 import * as React from "react";
 import { graphql } from "relay-runtime";
-import { useFragment } from "react-relay";
+import { useFragment, useRefetchableFragment } from "react-relay";
 import type { ContactsListFragment$key } from "./__generated__/ContactsListFragment.graphql";
 import Card from "./Card";
 import ContactRow from "./ContactRow";
+import SearchInput from './SearchInput';
 
 export type Props = {
   viewer: ContactsListFragment$key;
 };
 
 const ContactsListFragment = graphql`
-  fragment ContactsListFragment on Viewer {
-    contacts {
+  fragment ContactsListFragment on Viewer
+  @refetchable(queryName: "ContactsListRefetchQuery")
+  @argumentDefinitions(
+    search: {type: "String", defaultValue: null}
+  )
+  {
+    contacts(search: $search) {
       id
       ...ContactRowFragment
     }
@@ -19,10 +25,25 @@ const ContactsListFragment = graphql`
 `;
 
 export default function ContactsList({ viewer }: Props) {
-  const data = useFragment(ContactsListFragment, viewer);
+  const [isPending, startTransition] = React.useTransition();
+  const [data, refetch] = useRefetchableFragment(ContactsListFragment, viewer);
+  const [searchString, setSearchString] = React.useState<string>('');
+
+  const onSearchStringChanged = React.useCallback((value: string) => {
+    setSearchString(value);
+    startTransition(() => {
+      refetch({search: value});
+    });
+  }, [searchString])
+
   return (
     <Card dim={true}>
       <h3>Contacts</h3>
+      <SearchInput
+        value={searchString}
+        onChange={onSearchStringChanged}
+        isPending={isPending}
+      />
       {data.contacts.map((contact) => (
         <ContactRow key={contact.id} contact={contact} />
       ))}
--- newsfeed/src/components/PosterDetailsHovercardContents.tsx
+++ newsfeed/src/components/PosterDetailsHovercardContents.tsx
@@ -6,6 +6,7 @@ import Timestamp from "./Timestamp";
 
 import type { PosterDetailsHovercardContentsQuery as QueryType } from "./__generated__/PosterDetailsHovercardContentsQuery.graphql";
 import type { PosterDetailsHovercardContentsBodyFragment$key } from "./__generated__/PosterDetailsHovercardContentsBodyFragment.graphql";
+import OrganizationKind from "./OrganizationKind";
 
 export const PosterDetailsHovercardContentsQuery = graphql`
   query PosterDetailsHovercardContentsQuery ($posterID: ID!){
@@ -34,6 +35,14 @@ const PosterDetailsHovercardContentsBodyFragment = graphql`
     profilePicture {
       ...ImageFragment
     }
+    ... on Organization {
+      organizationKind
+    }
+    ... on Person {
+      location {
+        name
+      }
+    }
   }
 `;
 
@@ -56,6 +65,12 @@ function PosterDetailsHovercardContentsBody({
         <li>
           Joined <Timestamp time={data.joined} />
         </li>
+        {data.location != null && (
+           <li>{data.location.name}</li>
+         )}
+        {data.organizationKind != null && (
+          <li><OrganizationKind kind={data.organizationKind} /></li>
+         )}
       </ul>
       <div className="posterHovercard__buttons">
         <button>Friend</button>
